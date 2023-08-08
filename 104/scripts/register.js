cc8debeebe674ef9613a21558b1b52662f6ab04a
glendaandrews77 <138835509+glendaandrews77@users.noreply.github.com>
2023-08-07 17:57:52 -0700

    Add files via upload

--- 104/scripts/register.js
+++ 104/scripts/register.js
@@ -13,35 +13,91 @@ let PetSalon = {
         street:"23 Jones Street",
         zipcode:"23433"
     },
-    pets:[//petSalon.pets.name
-        {
-            name:"Taco",
-            age:4,
-            gender:"female",
-            service:"Grooming",
-        },
-        {
-        name: "Lovey",
-        age: 15,
-        gender: "Female",
-        service:"Nail cut",
-        }
-    
-    ]
+    pets:[]
+
+}
+
+//constructor
+function Pet(name,age,gender,breed){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+    this.services=service;
+
 }
 
 function displayPetName(){
         const petsDiv = document.querySelector("#pets");
-        for (let i = 0; i < PetSalon.pets.length; i ++) {
-            petsDiv.innerHTML +=` 
-            <li> ${PetSalon.pets[i].name}
-            </li>`;
-
+        petsDiv.innerHTML+""; //Clear previous list
+        for (let i = 0; i < PetSalon.pets.length; i++) {
+            petsDiv.innerHTML +=`<li> ${PetSalon.pets[i].name} </li>`;
         }
+}
+
+//Get the inputs{}
+let inputName = document.getElementById("txtName");
+let inputAge = document.getElementById("txtAge");
+let inputGender = document.getElementById("txtGender");
+let inputBreed = document.getElementById("txtBreed");
+let.inputService = document.getElementById("txtService");
+
+function register(){
+  //Create the new pet
+
+let newPet = new Pet(inputName.value, inputAge.value, inputGender.value, inputBreed.value,
+inputGender.value, inputBreed.value, inputServices.value);
+
+//  Add selected service to the pet
+newPet.services.push(inputService.value);
+
+
+
+
+//Push the new pet
+PetSalon.pets.push(newPet);
+console.log(PetSalon.pets);
+
+//Clear the form inputs
+function clearForm(){
+    inputName.value = "";
+    inputAge.value = "";
+    inputGender.value = "";
+    inputBreed.value = "";
+    inputService.value = "";
 
 }
+function deletePet(aPetID){
+
+    console.log("Deleting the pet" + aPetID);
+    document.getElementById(a.getElementById).removeAttribute();
+    for(let i=0; i<PetSalon.pets.length; i++){
+        let pet = petSalon.pets[i];
+        if(pet.id==aPetID){
+            deleteIndex=i;
+        }
+    }
+}
+
+inputName.focus();
+
+ // Refresh the pets list
+ displayPetName();
+
+ console.log(PetSalon.pets);
+}
 function init(){
-    displayPetName();
+    //create the pets objects
+
+    let taco = new Pet ("Taco",2,"female","Chihuahua",Grooming);
+    console.log(taco);
+    let lovey = new Pet ("Lovey",3,"female","Chihuahua",Grooming);
+    console.log(lovey);
+    let jaboi = new Pet ("JaBoi",4,"male","JackRussel",Grooming);
+    console.log(jaboi);
+    PetSalon.pets.push(taco,lovey,jaboi);
+    console.log(PetSalon.pets);
+
 }
 //waiting to render the html
 window.onload=init;
